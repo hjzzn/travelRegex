0a7409057e8e2735d5c54c840f849b3ef02b89ff
hjzzn <hjzzn@126.com>
2025-10-28 17:32:47 +0800

    new file

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import re
+
+
 def greet(name):
     print(f"Hello, {name}!")
 
 
 def modifyText():
-
-    symbol="!"
-    fo = open("new.txt", "w",encoding='utf-8')
-    with open('旅游流水账.txt', 'r', encoding='utf-8') as file:
+    symbol = "!"
+    fo = open("new.txt", "w", encoding='utf-8')
+    with open('湘西秋韵.txt', 'r', encoding='utf-8') as file:
         count = 0
         str1 = ""
         while True:
@@ -17,27 +18,30 @@ def modifyText():
                 break
 
             res1 = line.strip() + symbol
-            pattern1 =r'^(0\d|1[0-2])(-[0-2]\d|3[0-1])'
-            repl1=r'\n;\1\2!'
+            pattern1 = r'^(0\d|1[0-2])(-[0-2]\d|3[0-1])'
+            repl1 = r'\n\1\2!'
             # Search for the pattern
-            res2 = re.sub(pattern1,repl1,res1,re.IGNORECASE)
-            temp=re.match(pattern1, res1)
+            res2 = re.sub(pattern1, repl1, res1, re.IGNORECASE)
+            temp = re.match(pattern1, res1)
             if temp:
-                str1=temp.group(1)+temp.group(2)
-            pattern2 =  r'^([0-1]\d|2[0-4])(:[0-5]\d)'
-            repl2 = r'\n;' + str1 + r'!\1\2'
+                str1 = temp.group(1) + temp.group(2)
+            pattern2 = r'^([0-1]\d|2[0-4])(:[0-5]\d)'
+            repl2 = r'\n' + str1 + r'!\1\2'
             # Search for the pattern
-            res3 = re.sub(pattern2,repl2,res2,re.IGNORECASE)
+            res3 = re.sub(pattern2, repl2, res2, re.IGNORECASE)
             print(f"{res3}")
             fo.write(res3)
     fo.close()
+
 
 def main():
     # greet("World")
     # print("This code runs when the script is executed directly.")
     modifyText()
-#test in local
 
 
+# test in local
+
+print(__name__)
 if __name__ == "__main__":
     main()
